e7140045cf80273cd521df177855da7a0d4b7d26
aaaaaakjj <kazya1192@outlook.jp>
2023-09-16 17:47:39 +0900

    コメント投稿機能を実装しましょう

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,26 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    @comment.save
+
+
+    if  @comment.save
+       # 保存が成功した場合の処理
+    redirect_to 'prototype/show', notice: 'プロトタイプが正常に投稿されました。'
+  else
+    # 保存が失敗した場合の処理
+    render 'prototype/show', status: :unprocessable_entity
+  end
+
     redirect_to root_path
   end
+
+  if @prototype.save
+   
+
+
+
+
+
 
   private
 
